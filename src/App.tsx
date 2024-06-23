f7424bcea80b21d5f8bd45560546be9c19850a50
David Freixes <dfis1996@gmail.com>
2024-06-23 12:32:44 +0200

    New Header

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,14 @@ import TodoList from "./components/TodoList";
 import { Task } from "./model/task";
 import {v4 as uuidv4 } from 'uuid';
 import './App.css'
+import Header from "./components/Header";
 
 
 const App=()=> {
-  const[todos, setTodos]=useState<Task[]>([]);
-  const[completedTodos, setCompletedTodos] = useState<Task[]>([]);
+  const [todos, setTodos]=useState<Task[]>([]);
+  const [completedTodos, setCompletedTodos] = useState<Task[]>([]);
+  const [completedTasks, setCompletedTasks] = useState(0);
+  const [totalTasks, setTotalTasks] = useState(0);
 
   const handleAddTarea=(text:string)=>{
     const newTodo : Task = {
@@ -17,6 +20,7 @@ const App=()=> {
       completed: false,
     };
     setTodos([...todos,newTodo]);
+    setTotalTasks(totalTasks + 1);
   };  
 
   const handleToggleCompleted = (task: Task) => {
@@ -36,19 +40,24 @@ const App=()=> {
       setTodos(todos.filter((t) => t.id!== task.id));
       setCompletedTodos([...completedTodos, {...task, completed: true }]);
     }
+    if (!task.completed) {
+      setCompletedTasks(completedTasks + 1);
+    } else {
+      setCompletedTasks(completedTasks - 1);
+    }
   };
   
-   const handleDelete = (task: Task) => {
+  const handleDelete = (task: Task) => {
     if (task.completed) {
       setCompletedTodos(completedTodos.filter((t) => t.id !== task.id));
+      setCompletedTasks(completedTasks - 1);
     } else {
       setTodos(todos.filter((t) => t.id !== task.id));
     }
-    
-    setTodos(todos.filter((t)=>t.id != task.id))
-  };
+      setTotalTasks (totalTasks - 1)
+    };
 
-   const handleEditTask = (task: Task, newName: string) => {
+  const handleEditTask = (task: Task, newName: string) => {
     setTodos(
       todos.map((t) => {
         if (t.id === task.id) {
@@ -61,7 +70,11 @@ const App=()=> {
 
   return (
     <div>
-      <h1>Aplicacion de Tareas</h1>
+      <Header
+        completedTasks={completedTasks}
+        totalTasks={totalTasks}
+      />
+      <h2>Tareas a realizar:</h2>
       <TodoForm onAddTarea={handleAddTarea} />
       <div className="columns-container">
       <div className="column left-column">
